9b4e26a618955cb65394c2777c3051118e3a1670
mkwkamil <kamil.porebski@icloud.com>
2025-06-16 12:51:42 +0200

    Migrations

--- LoginComponentBackend/Program.cs
+++ LoginComponentBackend/Program.cs
@@ -1,4 +1,10 @@
+using LoginComponentBackend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
