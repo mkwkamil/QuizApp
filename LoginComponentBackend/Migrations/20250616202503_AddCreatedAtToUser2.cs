77a657d8f3a1ae4e5dfd6f93552cfd65f3c16d02
mkwkamil <kamil.porebski@icloud.com>
2025-06-16 22:30:28 +0200

    Migration updated

---
+++ LoginComponentBackend/Migrations/20250616202503_AddCreatedAtToUser2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginComponentBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToUser2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
