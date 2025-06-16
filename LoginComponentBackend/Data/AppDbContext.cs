3ac637c65947ce958dac7f393895c44955b88c84
mkwkamil <kamil.porebski@icloud.com>
2025-06-16 12:44:47 +0200

    Models updated

--- LoginComponentBackend/Data/AppDbContext.cs
+++ LoginComponentBackend/Data/AppDbContext.cs
@@ -1,6 +1,40 @@
+using LoginComponentBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace LoginComponentBackend.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
     
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Username)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.PasswordHash)
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.PasswordSalt)
+            .IsRequired();
+    }
 }
---
+++ LoginComponentBackend/Models/AuthResponse.cs
@@ -0,0 +1,8 @@
+namespace LoginComponentBackend.Models;
+
+public class AuthResponse
+{
+    public string Token { get; set; }
+    public string Username { get; set; }
+    public string Role { get; set; }
+}
