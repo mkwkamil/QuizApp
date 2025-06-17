c88b5b74800bed5a264ed35f73e1d272fd858163
mkwkamil <kamil.porebski@icloud.com>
2025-06-17 15:06:21 +0200

    Removed some dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-import './App.css';
 
 function App() {
     const [view, setView] = useState('login');
